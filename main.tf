b50593a4f981bfa60d05a62ece5c66a8cc25b01f
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-05-22 20:41:40 +0200

    Added gitignore and storage account configuration

--- main.tf
+++ main.tf
@@ -22,13 +22,28 @@ resource "azurerm_resource_group" "blob-site" {
 
 
 resource "azurerm_storage_account" "blob-site" {
-  name                     = "blob-site-storageaccount1234K"
+  name                     = "blobsitestorageaccount1"
   resource_group_name      = azurerm_resource_group.blob-site.name
   location                 = azurerm_resource_group.blob-site.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+  }
 
   tags = {
     environment = "Development"
   }
+}
+
+
+resource "azurerm_storage_blob" "blob-site" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.blob-site.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source_content         = "<h1>Hello World</h1>"
 }
