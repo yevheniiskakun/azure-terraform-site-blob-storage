f47d45a50f56052c33fac52d441ec61fdcc2c1a8
yevheniiskakun <yevhenii.skakun@gmail.com>
2025-06-05 22:57:01 +0200

    Added default values and corrected backend references

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tf-state-rg"
-    storage_account_name = "tfstateblobsite"
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "ststate897"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "location" {
   type        = string
   description = "Location where resources will be created"
+  default = "centralindia"
 }
 
 variable "resource_group_name" {
   type        = string
   description = "Resource group where storage account will be created"
+  default = "prod-rg-terraform-azure"
 }
 
 variable "storage_account_name" {
   type        = string
   description = "Name of the storage account where the site will be hosted"
+  default = "storageblobsite3456w"
 }
 
 variable "index_document" {
   type        = string
   description = "The name of the index document"
+  default = "index.html"
 }
 
 variable "source_content" {
   type        = string
   description = "Content of index file"
+  default = "<h1>Hello World</h1>"
 }
